ab3168da194d96587d061927212e984984be9e3a
Daniel Aristofanes Correia Matos <daniel.matos@s4r.com.br>
2023-04-05 22:49:54 -0300

    Modulo 8

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from 'src/helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  form: any = FormGroup;
 
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  public validation(): void{
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+    };
+
+    this.form = this.fb.group({
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', Validators.required],
+      senha: ['', [Validators.required,Validators.minLength(6)]],
+      confimeSenha: ['', Validators.required],
+    }, formOptions);
+  }
 }
